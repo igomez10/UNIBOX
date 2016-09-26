9eb2b4ea3585e85b0dc12498c37e9d17fbe43ad8
Ignacio Gómez <i.gomez10@uniandes.edu.co>
2016-09-25 21:48:08 -0500

    arreglado administrador repetido. el codigode la materia ahora incluye el programa

--- script.js
+++ script.js
@@ -9,202 +9,202 @@
 
 	var mate = [
 	{
-		 codigo:'1101'
+		 codigo:'ADMI1101'
 		,nombre:'FUNDAM.ADMON Y GERENCIA (ADMI)'
 	},
 	{
-		 codigo:'1102'
+		 codigo:'ADMI1102'
 		,nombre:'FUNDAM.ADMON Y GERENCIA'
 	},
 	{
-		 codigo:'1102C'
+		 codigo:'ADMI1102C'
 		,nombre:'FUNDAM.ADMON Y GERENCIA COMPL'
 	},
 	{
-		 codigo:'1190'
+		 codigo:'ADMI1190'
 		,nombre:'SERVICIOS ECOSISTÉMICOS'
 	},
 	{
-		 codigo:'1203'
+		 codigo:'ADMI1203'
 		,nombre:'FINANZAS 1: PLANEACION'
 	},
 	{
-		 codigo:'1401'
+		 codigo:'ADMI1401'
 		,nombre:'TALLER HABILIDADES INFORMATICA'
 	},
 	{
-		 codigo:'1590'
+		 codigo:'ADMI1590'
 		,nombre:'TALLER DE CREATIVIDAD'
 	},
 	{
-		 codigo:'1602'
+		 codigo:'ADMI1602'
 		,nombre:'TALLER AUTOCONOCIMIENTO'
 	},
 	{
-		 codigo:'1603'
+		 codigo:'ADMI1603'
 		,nombre:'COMPORT. ORGANIZACIONAL'
 	},
 	{
-		 codigo:'2104B'
+		 codigo:'ADMI2104B'
 		,nombre:'HISTORIA DESARROLLO EMPRES.COL'
 	},
 	{
-		 codigo:'2106'
+		 codigo:'ADMI2106'
 		,nombre:'RESPONSABILIDAD SOCIAL'
 	},
 	{
-		 codigo:'2107'
+		 codigo:'ADMI2107'
 		,nombre:'GERENCIA DE RECURSOS HUMANOS'
 	},
 	{
-		 codigo:'2204'
+		 codigo:'ADMI2204'
 		,nombre:'FINANZAS 2: INVERSION'
 	},
 	{
-		 codigo:'2205'
+		 codigo:'ADMI2205'
 		,nombre:'FINANZAS 3: ESTRATEGIA'
 	},
 	{
-		 codigo:'2206'
+		 codigo:'ADMI2206'
 		,nombre:'COMPORTAMIENTO DEL CONSUMIDOR'
 	},
 	{
-		 codigo:'2207'
+		 codigo:'ADMI2207'
 		,nombre:'COLOMBIA Y SUS INSTITUCIONES'
 	},
 	{
-		 codigo:'2301'
+		 codigo:'ADMI2301'
 		,nombre:'FUNDAMENTOS DE MERCADEO'
 	},
 	{
-		 codigo:'2302'
+		 codigo:'ADMI2302'
 		,nombre:'INVESTIGACION DE MERCADOS'
 	},
 	{
-		 codigo:'2403'
+		 codigo:'ADMI2403'
 		,nombre:'OPERACIONES Y LOGISTICA'
 	},
 	{
-		 codigo:'2501'
+		 codigo:'ADMI2501'
 		,nombre:'ANALISIS ENTORNO COLOMBIANO'
 	},
 	{
-		 codigo:'2557'
+		 codigo:'ADMI2557'
 		,nombre:'HERRAMIENTAS TOMA DECI II'
 	},
 	{
-		 codigo:'2605'
+		 codigo:'ADMI2605'
 		,nombre:'ORGANIZACIONES'
 	},
 	{
-		 codigo:'2801'
+		 codigo:'ADMI2801'
 		,nombre:'LA GESTION DE LO PUBLICO'
 	},
 	{
-		 codigo:'3107'
+		 codigo:'ADMI3107'
 		,nombre:'CASOS Y PROYECTOS EN ADMON'
 	},
 	{
-		 codigo:'3109'
+		 codigo:'ADMI3109'
 		,nombre:'PLANES DE NEGOCIOS'
 	},
 	{
-		 codigo:'3110'
+		 codigo:'ADMI3110'
 		,nombre:'EMPRENDIMIENTO E INNOVACION'
 	},
 	{
-		 codigo:'3122'
+		 codigo:'ADMI3122'
 		,nombre:'JUEGO GERENCIAL'
 	},
 	{
-		 codigo:'3133B'
+		 codigo:'ADMI3133B'
 		,nombre:'GERENCIA AMBIENTAL'
 	},
 	{
-		 codigo:'3137'
+		 codigo:'ADMI3137'
 		,nombre:'MODELOS DE LIDERAZGO'
 	},
 	{
-		 codigo:'3200'
+		 codigo:'ADMI3200'
 		,nombre:'OPCION DE GRADO'
 	},
 	{
-		 codigo:'3205'
+		 codigo:'ADMI3205'
 		,nombre:'FINANZAS PERSONALES'
 	},
 	{
-		 codigo:'3207'
+		 codigo:'ADMI3207'
 		,nombre:'MERCADO DE CAPITALES'
 	},
 	{
-		 codigo:'3210'
+		 codigo:'ADMI3210'
 		,nombre:'MERCADOS DERIVADOS'
 	},
 	{
-		 codigo:'3237'
+		 codigo:'ADMI3237'
 		,nombre:'MARKETING COMMUNICATIONS'
 	},
 	{
-		 codigo:'3313'
+		 codigo:'ADMI3313'
 		,nombre:'SALES AND DISTRIBUTION MANAGEM'
 	},
 	{
-		 codigo:'3327'
+		 codigo:'ADMI3327'
 		,nombre:'OPCIONES REALES'
 	},
 	{
-		 codigo:'3336'
+		 codigo:'ADMI3336'
 		,nombre:'MARKETING CULTURE AND THE ARTS'
 	},
 	{
-		 codigo:'3405'
+		 codigo:'ADMI3405'
 		,nombre:'SISTEMAS INFORMACION GERENCIAL'
 	},
 	{
-		 codigo:'3406'
+		 codigo:'ADMI3406'
 		,nombre:'INTEL.NEGOC.MERCAD.RELAC.(CRM)'
 	},
 	{
-		 codigo:'3431'
+		 codigo:'ADMI3431'
 		,nombre:'MARKETING DIGITAL'
 	},
 	{
-		 codigo:'3502'
+		 codigo:'ADMI3502'
 		,nombre:'ESTRATEGIA'
 	},
 	{
-		 codigo:'3503'
+		 codigo:'ADMI3503'
 		,nombre:'SERVICE MANAGEMENT'
 	},
 	{
-		 codigo:'3601'
+		 codigo:'ADMI3601'
 		,nombre:'TALL. HABILIDADES GERENC.'
 	},
 	{
-		 codigo:'3604'
+		 codigo:'ADMI3604'
 		,nombre:'TALLER DE INNOVACION'
 	},
 	{
-		 codigo:'3606'
+		 codigo:'ADMI3606'
 		,nombre:'ESTRATEGIAS DE NEGOCIACION'
 	},
 	{
-		 codigo:'3611'
+		 codigo:'ADMI3611'
 		,nombre:'ALIMENTOS Y OPORTUN.EMPRENDED.'
 	},
 	{
-		 codigo:'3615'
+		 codigo:'ADMI3615'
 		,nombre:'MERCADEO DEPORTIVO'
 	},
 	{
-		 codigo:'3701'
+		 codigo:'ADMI3701'
 		,nombre:'NEGOCIOS INTERNACIONALES'
 	},
 	{
-		 codigo:'3990'
+		 codigo:'ADMI3990'
 		,nombre:'INSCRIPCION A GRADO'
 	},
 	{
-		 codigo:'3991'
+		 codigo:'ADMI3991'
 		,nombre:'PRACTICA EMPRESARIAL'
 	}];
