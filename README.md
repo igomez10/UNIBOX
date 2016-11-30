930d46267f245eaf5bedb10dc024460c6f9576c9
Ignacio Gómez <i.gomez10@uniandes.edu.co>
2016-11-29 22:51:34 -0500

    0.6
    +BACKEND
    +node Fsapi added to the project
    
    PENDING:
    Upload files

---
+++ README.md
@@ -0,0 +1,210 @@
+# Node FileSystem API
+
+Node-FSAPI provides a RESTful (CRUD) server for interacting with remote file systems. It relies on 
+GET (Read), POST (Create), PUT (Update), and DELETE (Delete) commands with a plain-language syntax. 
+
+## Getting Started
+
+FSAPI provides a single-file `server.js` node controller with 2 core dependencies - [Restify](http://mcavage.github.io/node-restify) 
+and [node-fs-extra](https://github.com/jprichardson/node-fs-extra). The file contains a `config` object which allows for easy configuration.
+
+### Security
+
+The server provides 3 levels of security:
+
+1. Key-Based: Each request requires a key be submitted in the URL
+2. IP Restrictions: Supports specific IP addresses and ranges using wildcards (`*`)
+3. HTTPS Support: Simply supplying a PEM-encoded key and certificate file will require HTTPS requests
+
+### Configuration
+
+The server uses a `config` object to easily setup how it will run:
+
+**Keys**
+
+The `config.keys` array simply contains a list of keys that get sent along with the request.
+
+**IP Restrictions**
+
+The `config.ips` array allows providing a list of allowed IP addresses and ranges. Several format examples are:
+
+```
+"*.*.*.*"      // Allow all IP's through
+"192.168.*.*"  // Allow only IP's on the 192.168.(...) range to make requests
+"192.168.1.1"  // Allow only the specific address to make requests
+```
+
+**SSL Certificate**
+
+An SSL Certificate can be supplied by providing a PEM-encoded `key` and `cert`. Setting either or both of these properties to `false` results in no SSL.
+
+**Port**
+
+The `config.port` property sets the server port to be used.
+
+**Base Directory**
+
+The `config.base` property sets the base (or root) directory where files will reside. This is relative to the server file.
+
+**Create Mode**
+
+The `config.cmode` sets the permissions that will be applied on file creation. Default is `0755`.
+
+## Usage
+
+Requests to the server are made via RESTful methods - GET, PUT, POST and DELETE. Below is a breakdown of the methods and their associated methods:
+
+### GET (Read)
+
+**Directory Listing**
+
+`GET => {server}:{port}/{key}/dir/{path}`
+
+**Read File**
+
+`GET => {server}:{port}/{key}/file/{path}`
+
+### POST (Create)
+
+**Create Directory**
+
+`POST => {server}:{port}/{key}/dir/{path}`
+
+**Create File**
+
+`POST => {server}:{port}/{key}/file/{path}`
+
+**Copy Directory or File**
+
+`POST => {server}:{port}/{key}/copy/{path}`
+
+`POST` parameter `destination` required with the FULL detination path
+
+### PUT (Update)
+
+**Rename File or Directory**
+
+`PUT => {server}:{port}/{key}/rename/{path}`
+
+`PUT` parameter `name` required with the new file or directory name (no path required)
+
+**Save Contents to File**
+
+`PUT => {server}:{port}/{key}/save/{path}`
+
+`PUT` parameter `data` is required with the contents to be saved
+
+### DELETE
+
+**Delete a File or Directory**
+
+`DELETE => {server}:{port}/{key}/{path}`
+
+## Responses
+
+### Authentication Failure
+
+All authentication failures will result in an http 401 status (Not Authorized)
+
+### Success Response
+
+On a successful request the server will respond with the following JSON formatted return:
+
+```
+{
+  "status": "success",
+  "data": "{any return data}"
+}
+```
+
+Most successful responses will contain `null` for data.
+
+### Error Response
+
+On an erroroneous request the server will respond with the following JSON formatted return:
+
+```
+{
+  "status": "error",
+  "code": "{3-digit error response code}",
+  "message": "{brief explanation of error condition}",
+  "raw": "{raw error message from node}"
+}
+```
+
+## Working with the Client Methods
+
+The `client.js` file provides method for easily connecting to and interacting with the server.
+
+### Config
+
+Initially it is important to define the connection information, which is done through the following:
+
+```
+fsapi.config("http://yourserver:port", "api-key", {OPTIONAL - Bool 'Validate'});
+```
+
+The config process (with arguments) sets these values into localStorage (with Cookie fallback). There is a third argument `validate` which defaults to `true`. 
+If set to `false` in the config call above the entire response from the server will be returned and must be parsed manually.
+
+Calling `fsapi.config()` without arguments will return an object with the url and key. You can change either value individually using:
+
+```
+// Set new URL
+fsapi.store('fsapiUrl', {new-value});
+
+// Set new Key
+fsapi.store('fsapiKey', {new-value});
+```
+
+### Cleanup / Disconnect
+
+To remove the key and url from localStorage simply call `fsapi.disconnect();`.
+
+### Methods
+
+The following methods are natively available, but can easily be expanded upon:
+
+```
+// List Contents of Directory
+fsapi.list(path, callback);
+
+// Return Contents of File
+fsapi.open(path, callback);
+
+// Create a New File
+fsapi.createFile(path, callback);
+
+// Create a New Directory
+fsapi.createDirectory(path, callback);
+
+// Create a Copy of a File or Directory (recursive)
+fsapi.copy(path, destination, callback);
+
+// Move a File or Directory (Cut+Paste)
+fsapi.move(path, destination, callback);
+
+// Save Contents to a File
+fsapi.save(path, contents, callback);
+
+// Rename a File or Directory
+fsapi.rename(path, new_name, callback);
+
+// Delete a File or Directory
+fsapi.delete(path, callback);
+```
+
+Callbacks for each method returns the response from the server (validated by default, see below) by passing in the `res` argument.
+
+### Validate
+
+The fsapi validate method is used to parse the response from the server, this method is called by default unless changed in the `fsapi.config` method.
+
+```
+fsapi.validate(data);
+```
+
+For a sucessful response this method will return boolean `true`, or in cases such as `.list()` and `.open()` will return the 
+data from the response.
+
+On error or failure, the response from `.validate()` will be boolean `false`.
---
+++ client.js
@@ -0,0 +1,371 @@
+var fsapi = {
+    
+    /**
+     * Config
+     * 
+     * Set: fsapi.config({url}, {key}, {optional - 'validate'});
+     * Get: fsapi.config();
+     * 
+     */
+     
+    config: function () {
+        
+        if (arguments.length) {
+            // Set values
+            this.store("fsapiUrl", arguments[0]);
+            this.store("fsapiKey", arguments[1]);
+            
+            // Defaults validation to true
+            arguments[2] = arguments[2] || true;
+            this.store("fsapiValidate", arguments[2]);
+        } else {
+            return {
+               fsapiUrl: this.store("fsapiUrl"),
+               fsapiKey: this.store("fsapiKey"),
+               fsapiValidate: this.store("fsapiValidate")
+            };
+        }
+        
+    },
+    
+    /**
+     * Disconnect, remove local stores
+     */
+    
+    disconnect: function () {
+        this.store("fsapiUrl", null);
+        this.store("fsapiKey", null);
+    },
+    
+    /**
+     * Request
+     * 
+     * Makes request
+     */
+     
+    request: function (url, type, params, fn) {
+        var _this = this;
+        this.ajax({
+            url: url,
+            type: type,
+            data: params,
+            success: function (res) {
+                res = JSON.parse(res);
+                (_this.config().fsapiValidate) ? fn(_this.validate(res)) : fn(res);
+            },
+            error: function () {
+                fn("{ \"status\": \"failure\" }");
+            }
+        });
+    },
+    
+    /**
+     * Validate Response
+     */
+     
+    validate: function (res) {
+        if (res.status === "success") {
+            if (!res.data) {
+                // No data, but successful
+                return true;
+            } else {
+                // Success with data
+                return res.data;
+            }
+        } else {
+            console.log('FAIL');
+            return false;
+        }
+    },
+    
+    /**
+     * Read (GET)
+     */
+    
+    open: function (path, fn) {
+        var url = this.config().fsapiUrl + "/" + this.config().fsapiKey + "/file/" + path;
+        this.request(url, "GET", null, fn);
+    },
+    
+    list: function (path, fn) {
+        var url = this.config().fsapiUrl + "/" + this.config().fsapiKey + "/dir/" + path;
+        this.request(url, "GET", null, fn);
+    },
+    
+    /**
+     * Create (POST)
+     */
+    
+    // Create handler
+    create: function (path, type, fn) {
+        var url = this.config().fsapiUrl + "/" + this.config().fsapiKey + "/" + type + "/" + path;
+        this.request(url, "POST", null, fn);
+    },
+    
+    // Proxy for create (file)
+    createFile: function (path, fn) {
+        this.create(path, "file", fn);
+    },
+    
+    // Proxy for create (dir)
+    createDirectory: function (path, fn) {
+        this.create(path, "dir", fn);
+    },
+    
+    // Copy file or directory
+    copy: function (path, destination, fn) {
+        var url = this.config().fsapiUrl + "/" + this.config().fsapiKey + "/copy/" + path;
+        this.request(url, "POST", { destination: destination }, fn);
+    },
+    
+    // Performs copy, then delete original
+    move: function (path, destination, fn) {
+        var _this = this;
+        this.copy(path, destination, function (data) {
+            if (data.status === "success") {
+                _this.delete(path, fn);   
+            } else {
+                fn(data);
+            }
+        });
+    },
+    
+    /**
+     * Update (PUT)
+     */
+    
+    save: function (path, data, fn) {
+        var url = this.config().fsapiUrl + "/" + this.config().fsapiKey + "/save/" + path;
+        this.request(url, "PUT", { data: data }, fn);
+    },
+    
+    rename: function (path, name, fn) {
+        var url = this.config().fsapiUrl + "/" + this.config().fsapiKey + "/rename/" + path;
+        this.request(url, "PUT", { name: name }, fn);
+    },
+    
+    /**
+     * Delete (DELETE)
+     */
+    
+    delete: function (path, fn) {
+        var url = this.config().fsapiUrl + "/" + this.config().fsapiKey + "/"+ path;
+        this.request(url, "DELETE", { name: name }, fn);
+    },
+    
+    /**
+     * AJAX Handler
+     * 
+     * **Configuration Object:**
+     * 
+     * `url`: URL of request if not specified as first argument
+     * 
+     * `type`: Request method, defaults to `GET`
+     * 
+     * `async`: Run request asynchronously, defaults to `TRUE`
+     * 
+     * `cache`: Cache the request, defaults to `TRUE`
+     * 
+     * `data`: Object or JSON data passed through request
+     * 
+     * `success`: Function called on successful request
+     * 
+     * `error`: Function called on failure of request
+     * 
+     * `qsData`: Allows blocking (set `false`) of `data` add to URL for RESTful requests
+    */
+    
+    ajax: function() {
+
+        // Parent object for all parameters
+        var xhr = {};
+    
+        // Determine call structure: ajax(url, { params }); or ajax({ params });
+        if (arguments.length === 1) {
+            // All params passed as object
+            xhr = arguments[0];
+        } else {
+            // Populate xhr obj with second argument
+            xhr = arguments[1];
+            // Add first argument to xhr object as url
+            xhr.url = arguments[0];
+        }        
+    
+        // Parameters & Defaults
+        xhr.request = false;
+        xhr.type = xhr.type || "GET";
+        xhr.data = xhr.data || null;
+        if (xhr.qsData || !xhr.hasOwnProperty("qsData")) { xhr.qsData = true; } else { xhr.qsData = false; }
+        if (xhr.cache || !xhr.hasOwnProperty("cache")) { xhr.cache = true; } else { xhr.cache = false; }
+        if (xhr.async || !xhr.hasOwnProperty("async")) { xhr.async = true; } else { xhr.async = false; }
+        if (xhr.success && typeof xhr.success === "function") { xhr.success = xhr.success; } else { xhr.success = false; }
+        if (xhr.error && typeof xhr.error === "function") { xhr.error = xhr.error; } else { xhr.error = false; }
+        
+        // Format xhr.data & encode values
+        if (xhr.data) {
+            var param_count = 0,
+                name,
+                value,
+                tmp_data = xhr.data;
+            for (var param in tmp_data) {
+                if(tmp_data.hasOwnProperty(param)){
+                    name = encodeURIComponent(param);
+                    value = encodeURIComponent(tmp_data[param]);
+                    if (param_count === 0) {
+                        xhr.data = name + "=" + value;
+                    } else {
+                        xhr.data += "&" + name + "=" + value;
+                    }
+                    param_count++;
+                }
+            }
+            xhr.data = xhr.data;
+        }
+    
+        // Appends data to URL
+        function formatURL(data) {
+            var url_split = xhr.url.split("?");
+            if (url_split.length !== 1) {
+                xhr.url += "&" + data;
+            } else {
+                xhr.url += "?" + data;
+            }
+        }
+    
+        // Handle xhr.data on GET request type
+        if (xhr.data && xhr.type.toUpperCase() === "GET" && xhr.qsData) {
+            formatURL(xhr.data);
+        }
+    
+        // Check cache parameter, set URL param
+        if (!xhr.cache) {
+            formatURL(new Date().getTime());
+        }
+    
+        // Establish request
+        if (window.XMLHttpRequest) {
+            // Modern non-IE
+            xhr.request = new XMLHttpRequest();
+        } else if (window.ActiveXObject) {
+            // Internet Explorer
+            xhr.request = new ActiveXObject("Microsoft.XMLHTTP");
+        } else {
+            // No request object, break
+            return false;
+        }
+    
+        // Monitor ReadyState
+        xhr.request.onreadystatechange = function () {
+            if (xhr.request.readyState === 4) {
+                if (xhr.request.status === 200) {
+                    if (xhr.success) {
+                        // Returns responseText and request object
+                        xhr.success(xhr.request.responseText, xhr.request);
+                    }
+                } else {
+                    if (xhr.error) {
+                        // Returns request object
+                        xhr.error(xhr.request);
+                    }
+                }
+            }
+        };
+    
+        // Open Http Request connection
+        xhr.request.open(xhr.type, xhr.url, xhr.async);
+    
+        // Set request header for POST
+        if (xhr.type.toUpperCase() === "POST" || xhr.type.toUpperCase() === "PUT") {
+            xhr.request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        }
+    
+        // Send data
+        xhr.request.send(xhr.data);
+    
+    },
+    
+    /**
+     * LocalStorage with polyfill support via cookies
+     * 
+     * @param {String} key The key or identifier for the store
+     * @param {String|Object} [value] Contents of the store, blank to return, "null" to clear
+     * 
+     * Specify a string/object value to `set`, none to `get`, and "null" to `clear`
+     */
+    store: function (key, value) {
+
+        var _this = this,
+            lsSupport = false;
+
+        // Check for native support
+        if (localStorage) {
+            lsSupport = true;
+        }
+
+        // If value is detected, set new or modify store
+        if (typeof value !== "undefined" && value !== null) {
+            // Stringify objects
+            if(typeof value === "object") {
+                value = JSON.stringify(value);
+            }
+            // Add to / modify storage
+            if (lsSupport) { // Native support
+                localStorage.setItem(key, value);
+            } else { // Use Cookie
+                _this.createCookie(key, value, 30);
+            }
+        }
+
+        // No value supplied, return value
+        if (typeof value === "undefined") {
+            if (lsSupport) { // Native support
+                return localStorage.getItem(key);
+            } else { // Use cookie
+                return _this.readCookie(key);
+            }
+        }
+
+        // Null specified, remove store
+        if (value === null) {
+            if (lsSupport) { // Native support
+                localStorage.removeItem(key);
+            } else { // Use cookie
+                _this.createCookie(key, "", -1);
+            }
+        }
+
+    },
+    
+    /**
+     * Creates new cookie or removes cookie with negative expiration
+     * 
+     * @param {String} key The key or identifier for the store
+     * @param {String} value Contents of the store
+     * @param {Number} exp Expiration in days
+     */
+    createCookie: function(key, value, exp) {
+        var date = new Date(),
+            expires;
+        date.setTime(date.getTime() + (exp * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+        document.cookie = key + "=" + value + expires + "; path=/";
+    },
+    
+    /**
+     * Reads cookie
+     * 
+     * @param {String} key The key or identifier for the store
+     * @return {String} the value of the cookie
+     */
+    readCookie: function(key) {
+        var nameEQ = key + "=",
+            ca = document.cookie.split(";");
+        for (var i = 0, max = ca.length; i < max; i++) {
+            var c = ca[i];
+            while (c.charAt(0) === " ") { c = c.substring(1, c.length); }
+            if (c.indexOf(nameEQ) === 0) { return c.substring(nameEQ.length, c.length); }
+        }
+        return null;
+    }  
+    
+};
